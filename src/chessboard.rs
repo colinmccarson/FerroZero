69367e8e7b91b3ca6f4f99fac3d560e4eeb3faca
Colin Jerome McCarson <colin.jerome.mccarson@gmail.com>
2025-08-19 23:26:56 -0500

    branchless bitboard access

--- src/chessboard.rs
+++ src/chessboard.rs
@@ -15,38 +15,40 @@ const INDEX64: [u32; 64] = [
 ];
 
 
+/** The following return zero when there is an error **/
 #[inline(always)]
 fn trailing_zeros_debruijn(x: u64) -> u64 {
     debug_assert!(x != 0 && x.count_ones() == 1);
-    let idx = ((x.wrapping_mul(DEBRUIJN64)) >> 58);
+    let idx = (x.wrapping_mul(DEBRUIJN64)) >> 58;
     INDEX64[idx]
 }
 
 
 #[inline(always)]
-fn increment_rank_saturating(source: u64) -> u64 {
-    ((!RANK_8 & source) << 8) | (source & RANK_8)
+fn increment_rank(source: u64) -> u64 {
+    (!RANK_8 & source) << 8
 }
 
 
 #[inline(always)]
-fn decrement_rank_saturating(source: u64) -> u64 {
-    ((!RANK_1 & source) >> 8) | (source & RANK_1)
+fn decrement_rank(source: u64) -> u64 {
+    (!RANK_1 & source) >> 8
 }
 
 
 #[inline(always)]
 fn move_right(source: u64) -> u64 {
-    (source & !FILE_H) << 1 | (source & FILE_H)
+    (source & !FILE_H) << 1
 }
 
 
 #[inline(always)]
 fn move_left(source: u64) -> u64 {
-    (source & !FILE_A) >> 1 | (source & FILE_A)
+    (source & !FILE_A) >> 1
 }
 
 
+#[repr(usize)]  // guarantees discriminant values start from 0
 pub enum ChesspieceType {
     WhiteRooks,
     WhiteKnights,
@@ -62,21 +64,31 @@ pub enum ChesspieceType {
     BlackPawns,
 }
 
+impl ChesspieceType {
+    #[inline]
+    pub fn index(self) -> usize {
+        self as usize
+    }
 
-#[derive(Clone)]
-pub struct Chessboard {
-    white_rooks: u64,
-    white_knights: u64,
-    white_bishops: u64,
-    white_queens: u64,
-    white_king: u64,
-    white_pawns: u64,
-    black_rooks: u64,
-    black_knights: u64,
-    black_bishops: u64,
-    black_queens: u64,
-    black_king: u64,
-    black_pawns: u64,
+    pub const ALL: [ChesspieceType; 12] = [
+        ChesspieceType::WhiteRooks,
+        ChesspieceType::WhiteKnights,
+        ChesspieceType::WhiteBishops,
+        ChesspieceType::WhiteQueens,
+        ChesspieceType::WhiteKing,
+        ChesspieceType::WhitePawns,
+        ChesspieceType::BlackRooks,
+        ChesspieceType::BlackKnights,
+        ChesspieceType::BlackBishops,
+        ChesspieceType::BlackQueens,
+        ChesspieceType::BlackKing,
+        ChesspieceType::BlackPawns,
+    ];
+}
+
+
+struct Chessboard {
+    pieces: [u64; 12],
 }
 
 
@@ -86,53 +98,17 @@ impl Chessboard {
     **/
     pub fn new() -> Chessboard {
         Chessboard {
-            white_rooks: 0,
-            white_knights: 0,
-            white_bishops: 0,
-            white_queens: 0,
-            white_king: 0,
-            white_pawns: 0,
-            black_rooks: 0,
-            black_knights: 0,
-            black_bishops: 0,
-            black_queens: 0,
-            black_king: 0,
-            black_pawns: 0,
+            pieces: [0; 12],
         }
     }
 
-    pub fn get(&self, bb: ChesspieceType) -> u64 {
-        match bb {
-            ChesspieceType::WhiteRooks => self.white_rooks,
-            ChesspieceType::WhiteKnights => self.white_knights,
-            ChesspieceType::WhiteBishops => self.white_bishops,
-            ChesspieceType::WhiteQueens => self.white_queens,
-            ChesspieceType::WhiteKing => self.white_king,
-            ChesspieceType::WhitePawns => self.white_pawns,
-            ChesspieceType::BlackRooks => self.black_rooks,
-            ChesspieceType::BlackKnights => self.black_knights,
-            ChesspieceType::BlackBishops => self.black_bishops,
-            ChesspieceType::BlackQueens => self.black_queens,
-            ChesspieceType::BlackKing => self.black_king,
-            ChesspieceType::BlackPawns => self.black_pawns,
-        }
+    #[inline]
+    pub fn get(&self, piece: ChesspieceType) -> u64 {
+        self.pieces[piece.index()]
     }
 
-    pub fn get_mut(&mut self, bb: ChesspieceType) -> &mut u64 {
-        match bb {
-            ChesspieceType::WhiteRooks => &mut self.white_rooks,
-            ChesspieceType::WhiteKnights => &mut self.white_knights,
-            ChesspieceType::WhiteBishops => &mut self.white_bishops,
-            ChesspieceType::WhiteQueens => &mut self.white_queens,
-            ChesspieceType::WhiteKing => &mut self.white_king,
-            ChesspieceType::WhitePawns => &mut self.white_pawns,
-            ChesspieceType::BlackRooks => &mut self.black_rooks,
-            ChesspieceType::BlackKnights => &mut self.black_knights,
-            ChesspieceType::BlackBishops => &mut self.black_bishops,
-            ChesspieceType::BlackQueens => &mut self.black_queens,
-            ChesspieceType::BlackKing => &mut self.black_king,
-            ChesspieceType::BlackPawns => &mut self.black_pawns,
-        }
+    #[inline]
+    pub fn get_mut(&mut self, piece: ChesspieceType) -> &mut u64 {
+        &mut self.pieces[piece.index()]
     }
-
 }
