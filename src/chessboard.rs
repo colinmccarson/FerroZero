19f48757d542ca639d36959a778c3643066aee85
Colin Jerome McCarson <colin.jerome.mccarson@gmail.com>
2025-08-23 21:30:40 -0500

    chess board tests

--- src/chessboard.rs
+++ src/chessboard.rs
@@ -1,9 +1,9 @@
+use crate::chess_attacks as chess_attacks;
+
 const DEBRUIJN64: u64 = 0x03f79d71b4cb0a89;
 const RANK_8: u64 = 0xFF00_0000_0000_0000;
 const RANK_1: u64 = 0x0000_0000_0000_00FF;
-const FILE_A: u64 = 0x0101_0101_0101_0101;
-const FILE_H: u64 = 0x8080_8080_8080_8080;
-const INDEX64: [u32; 64] = [
+const INDEX64: [u64; 64] = [
     0,  1, 48,  2, 57, 49, 28,  3,
     61, 58, 50, 42, 38, 29, 17,  4,
     62, 55, 59, 36, 53, 51, 43, 22,
@@ -17,34 +17,37 @@ const INDEX64: [u32; 64] = [
 
 /** The following return zero when there is an error **/
 #[inline(always)]
-fn trailing_zeros_debruijn(x: u64) -> u64 {
-    debug_assert!(x != 0 && x.count_ones() == 1);
-    let idx = (x.wrapping_mul(DEBRUIJN64)) >> 58;
+pub const fn trailing_zeros_debruijn(x: u64) -> u64 {
+    debug_assert!(x != 0);
+    debug_assert!(x.count_ones() == 1);
+    let idx: usize = ((x.wrapping_mul(DEBRUIJN64)) >> 58) as usize;
     INDEX64[idx]
 }
 
 
 #[inline(always)]
-fn increment_rank(source: u64) -> u64 {
-    (!RANK_8 & source) << 8
+pub const fn increment_rank(source: u64, num_ranks: u64) -> u64 {
+    (!RANK_8 & source) << (num_ranks << 3)
 }
 
 
 #[inline(always)]
-fn decrement_rank(source: u64) -> u64 {
-    (!RANK_1 & source) >> 8
+pub const fn decrement_rank(source: u64, num_ranks: u64) -> u64 {
+    (!RANK_1 & source) >> (num_ranks << 3)
 }
 
 
 #[inline(always)]
-fn move_right(source: u64) -> u64 {
-    (source & !FILE_H) << 1
+pub const fn move_right(source: u64, num_files: u64) -> u64 {
+    let row = 0xFFu64 << (((source.trailing_zeros() & 0x3F) >> 3) << 3);
+    (source >> num_files) & row
 }
 
 
 #[inline(always)]
-fn move_left(source: u64) -> u64 {
-    (source & !FILE_A) >> 1
+pub const fn move_left(source: u64, num_files: u64) -> u64 {
+    let row = 0xFFu64 << (((source.trailing_zeros() & 0x3F) >> 3) << 3);
+    (source << num_files) & row
 }
 
 
@@ -105,5 +108,16 @@ impl Chessboard {
     #[inline]
     pub fn get_mut(&mut self, piece: ChesspieceOffset) -> &mut u64 {
         &mut self.pieces[piece.index()]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_increment_rank() {
+        assert_eq!(increment_rank(2, 1), 0x00000200);
+        assert_eq!(decrement_rank(2, 3), 0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use crate::chess_attacks::rook_single;
+use crate::util::print_u64_as_8x8_bit_string;
+
 mod chessboard;
 mod pieces;
 mod chess_attacks;
+mod util;
 
 fn main() {
-    println!("Hello, world!");
+    println!("WIP!")
 }
